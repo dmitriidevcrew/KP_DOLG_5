fe25129b39a0e9a6f9a7f39b7c980879946b24ae
dmitriidevcrew <dmitrii.pavlovich.dev@bk.ru>
2023-12-04 02:59:40 +0300

    Добавьте файлы проекта.

---
+++ KP_DOLG_5/MyClass.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KP_DOLG_5
+{
+    public class MyClass
+    {
+
+        public int MySum(int a, int b, int c)
+        {
+            return a + b + c;
+        }
+
+        public int MyPow(int a)
+        {
+            return a * a * a;
+        }
+
+        public int MyMax(int a, int b)
+        {
+            return a > b ? a : b;
+        }
+    }
+}
---
+++ KP_DOLG_5/Program.cs
@@ -0,0 +1,23 @@
+﻿
+using KP_DOLG_5;
+
+MyClass myClass = new MyClass();
+Console.WriteLine("Введите 3 значения int для подсчета суммы элементов : ");
+Console.WriteLine("a:");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("b:");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("c:");
+int c = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Сумма 3 элементов: " + myClass.MySum(a, b, c));
+
+Console.WriteLine("Введите значение int для возведения в 3 степень : ");
+int pow = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Значение в 3 степени: " + myClass.MyPow(pow));
+
+Console.WriteLine("Введите 2 значение для получения наибольшего: ");
+Console.WriteLine("a:");
+int maxA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("b:");
+int maxB = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Наибольшее значение: " + myClass.MyMax(maxA, maxB));
